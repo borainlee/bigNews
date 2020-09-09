42a6dec40712e4b62058bc4bfff7a6ebb3e33526
borainlee <741567165@qq.com>
2020-09-09 17:15:55 +0800

    登录和注册验证规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,4 +9,25 @@ $(function () {
         $('.reg-box').hide()
         $('.login-box').show()
     })
+
+    // 从layui中获取form对象
+    var form = layui.form
+    // 通过form.verify()函数自定义校验规则
+    form.verify({
+        // 自定义一个叫做pwd校验规则
+        password: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 校验两次密码是否一致
+
+        // value表示使用了这个规则的表单中的属性值
+        repassword: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败，则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致!'
+            }
+        }
+    })
 })
